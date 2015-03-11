3bc82dd0b2958b5196d81380fdd26b80d348966f
Béuren F. Bechlin <beuren.bechlin@gmail.com>
2015-03-11 01:09:28 -0300

    Atualizado o protocolo de envio de dados e adicionado a segunda medida

--- python/armazenamento.py
+++ python/armazenamento.py
@@ -5,6 +5,8 @@
 #
 # Programa para aquisição dos dados
 # Este programa é utilizado com o programa biometrica.ino no qual a taxa de amostragem é definida no microcontrolador
+#
+# Protocolo usado está disponível no biométrica.ino
 # 
 # Centro de Tecnologia Acadêmica - UFRGS
 # http://cta.if.ufrgs.br
@@ -27,38 +29,46 @@ ser = serial.Serial(argumento[0], argumento[1])
 ser.setDTR(False)	# Desliga DTR
 time.sleep(0.005)  
 ser.flushInput()	# Limpa buffer de dados
-ser.setDTR(True)  	# Liga DTR novamente 
+ser.setDTR(True)  # Liga DTR novamente 
 
 # Variáveis de controle
 frequencia = int(ser.readline().replace('\r\n',''))
-periodo = 1.0/frequencia
 
 # Arquivos de log
 fisiologfile = open(argumento[2],'w')
 now=datetime.datetime.now()
-fisiologfile.write(now.strftime("#Frequência\t"+str(frequencia)+"\n"+"#Estação Biométrica\n"+"#Coleta de dados iniciado em: "+"%Y-%m-%d %H:%M:%S" +"\n"))
+fisiologfile.write(now.strftime("#Frequência\t"+str(frequencia)+"\n"+"#Estação Biométrica\n"+"#Coleta de dados iniciado em: "+"%Y-%m-%d %H:%M:%S" +"\n"+"#Protocolo usado: a primeira coluna é a medida do primeiro termistor e a segunda do segundo.\n"))
+fisiologfile.close()
+while 1:
+	try:
+		option = ser.read()
+		# Respiração
+		# 	Para o python '\x' serve para representar uma caracter HEX ASCII
+		# 	e o 0x para para valores HEX literais. O comando write do arduino
+		#	  envia um caracter, logo deve-se usar a primeira forma
+		if option == '\x0A': 
 
-while True:
-	try:
-    		# Temperatura
-    		t = ser.readline().replace('\r\n','')
-    		if t == 'Erro1':
-				print 'Frequência muito alta para o microcontrolador: ', frequencia, '. Finalizando processo.\n' 
-  	except KeyboardInterrupt:
-    		break
-	
-	try:
-		# Registra horário atual
-		now = datetime.datetime.now()
+			sensor_value1 = ser.readline().replace('\r\n','')
+			sensor_value2 = ser.readline().replace('\r\n','')
+
+			# Arquivo de log
+			fisiologfile = open(argumento[2],'a')
+			fisiologfile.write(str(sensor_value1)+"\t"+str(sensor_value2)+"\n")   
+			fisiologfile.close()
 		
-		# Mostrando dados na tela
-		print now.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3],"\t", t
- 
-		# Arquivo de log
-		fisiologfile = open(argumento[2],'a')
-		fisiologfile.write(str(t)+"\n")   
-		fisiologfile.close()
+			# Registra horário atual
+			now = datetime.datetime.now()
+				
+			# Mostrando dados na tela
+			print now.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3],"\t", sensor_value1, "\t", sensor_value2
+
+		# Erro 1
+		if option == '\x01':
+			print 'Frequência muito alta para o microcontrolador: ', frequencia, '. Finalizando processo.\n' 
+
+		
+	
 	except KeyboardInterrupt:
-    		break
-    		
+				break
+				
 ser.close()

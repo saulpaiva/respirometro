7cc6f1b6faf539ed707514933f320118efc69d32
Béuren F. Bechlin <beuren.bechlin@ufrgs.br>
2016-03-16 18:02:50 -0300

    Criação de interação e dockW

--- tests/qt_mpl/main.py
+++ tests/qt_mpl/main.py
@@ -27,15 +27,21 @@ class MyMplCanvas(FigureCanvasQTAgg):
 
         # inicializa FigureCanvasQTAgg com um objeto Figure
         super().__init__(self.fig)
-        self.plot()
+        # self.plot()
 
-    def plot(self):
-        t = arange(0.0, 3.0, 0.01)
-        s = sin(2*pi*t)
-        self.axes.plot(t, s)
+    def plot(self, R1, R2, RXmin, RXmax, V0):
+        # print(self.parent().parent().)
+        # print('baka', self.parent().parent().r1_edit.text())
+
+        def f(RX, R1, R2, V0):
+            return V0*(R1/(R1+R2) - RX/(R1+RX))
+        RX = arange(RXmin, RXmax, (RXmax - RXmin)/200)
+        V = f(RX, R1, R2, V0)
+        self.axes.plot(RX, V)
+        self.draw()
 
 
-class MyWindow(qt.QWidget):
+class MyWindow(qt.QMainWindow):
     '''
     Janela principal.
     '''
@@ -52,58 +58,80 @@ class MyWindow(qt.QWidget):
         mpl_layout.addWidget(self.mpl_canvas)
         mpl_layout.addWidget(self.mpl_toolbar)
 
+
+        mpl_area = qt.QWidget()
+        mpl_area.setLayout(mpl_layout)
+        self.setCentralWidget(mpl_area)
+        self.config_dock = self.initDockArea()
+        self.addDockWidget(qt.Qt.LeftDockWidgetArea, self.config_dock)
+
+
+    def initDockArea(self):
         # Config Area
-        config_area = qt.QWidget()
-        config_area.setFixedWidth(200)
+        config_dock = qt.QDockWidget()
+        config_dock.setFixedWidth(200)
 
         config_toolbar_grid = qt.QGridLayout()
         config_toolbar_grid.setSpacing(10)
 
-        equation_label = qt.QLabel('V*[R1/(R1+R2) - RX/(R1+RX)]')
+        equation_label = qt.QSvgWidget('CodeCogsEqn.svg')
 
         r1_label = qt.QLabel('R1:')
-        r1_edit = qt.QLineEdit()
+        self.r1_edit = qt.QLineEdit()
+        self.r1_edit.textChanged[str].connect(self.generateBttClicked)
 
         r2_label = qt.QLabel('R2:')
-        r2_edit = qt.QLineEdit()
+        self.r2_edit = qt.QLineEdit()
 
         rx_label = qt.QLabel('RX:')
-        rx_edit = qt.QLineEdit()
 
         min_label = qt.QLabel('Min')
-        min_edit = qt.QLineEdit()
+        self.min_edit = qt.QLineEdit()
         max_label = qt.QLabel('Max')
-        max_edit = qt.QLineEdit()
+        self.max_edit = qt.QLineEdit()
 
         volt_label = qt.QLabel('V:')
-        volt_edit = qt.QLineEdit()
+        self.volt_edit = qt.QLineEdit()
 
         generate_btt = qt.QPushButton('Generate')
+        generate_btt.clicked.connect(self.generateBttClicked)
 
         config_toolbar_grid.addWidget(equation_label, 0, 0, 1, 2)
         config_toolbar_grid.addWidget(r1_label, 1, 0)
-        config_toolbar_grid.addWidget(r1_edit, 1, 1)
+        config_toolbar_grid.addWidget(self.r1_edit, 1, 1)
         config_toolbar_grid.addWidget(r2_label, 2, 0)
-        config_toolbar_grid.addWidget(r2_edit, 2, 1)
+        config_toolbar_grid.addWidget(self.r2_edit, 2, 1)
         config_toolbar_grid.addWidget(rx_label, 3, 0, 1, 2)
         config_toolbar_grid.addWidget(min_label, 4, 0)
-        config_toolbar_grid.addWidget(min_edit, 4, 1)
+        config_toolbar_grid.addWidget(self.min_edit, 4, 1)
         config_toolbar_grid.addWidget(max_label, 5, 0)
-        config_toolbar_grid.addWidget(max_edit, 5, 1)
+        config_toolbar_grid.addWidget(self.max_edit, 5, 1)
         config_toolbar_grid.addWidget(volt_label, 6, 0)
-        config_toolbar_grid.addWidget(volt_edit, 6, 1)
-        
+        config_toolbar_grid.addWidget(self.volt_edit, 6, 1)
+
         config_layout = qt.QVBoxLayout()
         config_layout.addLayout(config_toolbar_grid)
         config_layout.addStretch()
         config_layout.addWidget(generate_btt)
 
-        # Main Area
-        main_layout = qt.QGridLayout()
-        main_layout.addLayout(mpl_layout, 0, 0, 1, 2)
-        main_layout.addLayout(config_layout, 0, 2, 1, 1)
-        self.setLayout(main_layout)
-        
+        config_widget = qt.QWidget()
+        config_widget.setLayout(config_layout)
+        config_dock.setWidget(config_widget)
+
+        return config_dock
+
+    def generateBttClicked(self):
+        #try:
+            self.mpl_canvas.plot(
+                float(self.r1_edit.text()),
+                float(self.r2_edit.text()),
+                float(self.min_edit.text()),
+                float(self.max_edit.text()),
+                float(self.volt_edit.text())
+                )
+        # except:
+        #     pass
+
     def keyPressEvent(self, event):
         super().keyPressEvent(event)
         if event.key() == qt.Qt.Key_Escape:
